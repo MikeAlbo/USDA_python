c08bc17880b8f0681dd881d6cfa7f792ac848206
Michael Albonetti <malbo24@gmail.com>
2019-05-23 11:05:54 -0600

    second attept at clean up function, trying to increase speed by loading everything into memory

--- cross_check_data.py
+++ cross_check_data.py
@@ -1,16 +1,5 @@
-
 import sqlite3
 
-print('''-
-Product Table cleanup
-This "cleanup" will remove products from the the Products table
-that do not have corresponding data in rhw Serving_size or Nutrition tables
-** must run the three parsers first or your gonna have a bad time! ** 
-''')
-
-if input("Ready to go? (y/n) ").lower() != "y":
-    print("bye, bye now...")
-    quit()
 
 db_connection = sqlite3.connect("../db/USDA2.sqlite")
 cur = db_connection.cursor()
@@ -25,48 +14,59 @@ cur.execute('''
     )
 ''')
 
+cur.execute('SELECT product_id FROM Products')
+product_ids = cur.fetchall()
 
-count = 0
+cur.execute('SELECT product_id FROM Nutrients')
+nutrition_ids = cur.fetchall()
+
+cur.execute('SELECT product_id FROM Serving_sizes')
+serving_sizes_ids = cur.fetchall()
+
+bad_count = 0
+count_down = len(product_ids)
 product_rows_deleted = 0
+rows_not_deleted = 0
 
 
 def move_row(move_product_id):
     try:
         cur.execute('SELECT * FROM Products WHERE product_id=? LIMIT 1', (move_product_id,))
         moved_product = cur.fetchone()
+        print(moved_product)
         cur.execute('''INSERT OR IGNORE INTO Deleted_Products 
                     (product_id, ndb_number, long_name, gtin_upc, manufacture_id, ingredients_id)
-                    VALUES (?, ?, ?, ?, ?)''', (moved_product[0], moved_product[1], moved_product[2], moved_product[3], moved_product[4], moved_product[5]))
+                    VALUES (?, ?, ?, ?, ?, ?)''', (moved_product[0], moved_product[1], moved_product[2], moved_product[3], moved_product[4], moved_product[5]))
         cur.execute('''DELETE FROM Products WHERE product_id=?''', (move_product_id,))
 
         if product_rows_deleted % 10 == 0:
             db_connection.commit()
 
+        return 0
+
     except:
         print("row not moved")
+        return 1
 
 
-cur.execute('SELECT product_id FROM Products')
-product_id_column = cur.fetchall()
+print("retrieving data...")
 
-for product_id in product_id_column:
 
-    p_id = product_id[0]
-
-    try:
-        cur.execute('SELECT * FROM Nutrients WHERE product_id=? LIMIT 1', (p_id,))
-        _ = cur.fetchone()
-        cur.execute('SELECT * FROM Serving_sizes WHERE product_id=? LIMIT 1', (p_id,))
-        _ = cur.fetchone()
-        print(p_id, "- ok")
-    except TypeError:
+for p_id in product_ids:
+    count_down -= 1
+    if p_id in nutrition_ids and p_id in serving_sizes_ids:
+        print(count_down)
+        continue
+    else:
+        bad_count += 1
         product_rows_deleted += 1
-        print("\n", p_id, "- removed\n ")
-        move_row(p_id)
+        rows_not_deleted += move_row(p_id[0])
 
-
-print("in", count)
-print("not in", product_rows_deleted)
 db_connection.commit()
+print("\n\n----- done -----\n\n")
+print("bad: ", bad_count)
+print("rows deleted:", product_rows_deleted)
+print("rows needed deleted but not: ", rows_not_deleted)
+
 
 db_connection.close()
